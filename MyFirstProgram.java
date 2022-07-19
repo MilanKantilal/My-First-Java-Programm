acb0809bb2e9a1306160e1e7a53a7c1269e6d88b
Milan <mkantilal@gmail.com>
2022-07-19 21:19:45 +0100

    my name is milankuamr

---
+++ MyFirstProgram.java
@@ -0,0 +1,7 @@
+public class MyFirstProgram {
+    public static void main (String[] args)
+    {
+        System.out.println("My First Application");
+        System.out.println("My name is Milankumar");// This is for test only
+    }
+}
